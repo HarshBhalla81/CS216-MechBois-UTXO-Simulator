7eae008c44f671e680c17c903e7e0f2144e749fc
HarshBhalla81 <bhallaharsh81@gmail.com>
2026-01-25 18:18:03 +0530

    transaction.py

---
+++ src/transaction.py
@@ -0,0 +1,20 @@
+transaction = {
+    "tx_id": "",
+    "inputs": [
+        {
+            "prev_tx": "",
+            "index": "",
+            "owner": ""
+        }
+    ],
+    "outputs": [
+        {
+            "amount": float,
+            "address": ""
+        }, 
+        {
+            "change": float,
+            "address": ""
+        }
+    ]
+}
